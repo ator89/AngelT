55238ba866ab02563376f0b76fb7c688691b7cc7
ator89 <ator89@outlook.com>
2018-02-02 14:18:51 -0600

    Hello world
    Detectives- Constructores

---
+++ Lab2_AngelT/src/lab2_angelt/Detectives.java
@@ -0,0 +1,81 @@
+
+
+package lab2_angelt;
+
+
+public class Detectives {
+    
+    private String nombre, nacionalidad, user, password;
+    private int edad,anios_trabajo;
+
+    public Detectives() {
+    }
+    
+    public Detectives(
+            String nombre, int edad, String nacionalidad,
+            int anios_trabajo, String user, String password){
+        this.nombre=nombre;
+        this.edad=edad;
+        this.nacionalidad=nacionalidad;
+        this.anios_trabajo=anios_trabajo;
+        this.user=user;
+        this.password=password;
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getAnios_trabajo() {
+        return anios_trabajo;
+    }
+
+    public void setAnios_trabajo(int anios_trabajo) {
+        this.anios_trabajo = anios_trabajo;
+    }
+
+    @Override
+    public String toString() {
+        return "Detectives{" + "nombre=" + nombre + ", nacionalidad=" + nacionalidad + ", user=" + user + ", password=" + password + ", edad=" + edad + ", anios_trabajo=" + anios_trabajo + '}';
+    }
+    
+    
+
+}
--- Lab2_AngelT/src/lab2_angelt/Lab2_AngelT.java
+++ Lab2_AngelT/src/lab2_angelt/Lab2_AngelT.java
@@ -1,4 +1,3 @@
-
 package lab2_angelt;
 
 import java.util.ArrayList;
@@ -7,68 +6,68 @@ import java.util.Scanner;
 public class Lab2_AngelT {
 
     public static void main(String[] args) {
-        
-        Scanner sc = new Scanner (System.in);
+
+        Scanner sc = new Scanner(System.in);
         ArrayList detectives = new ArrayList();
-        
+
         int opcion;
-        
-        do{
-            System.out.println("Dectectives\n"+
-                                "1 - Agregar detectives\n"+
-                                "2 - Eliminar detectives\n"+
-                                "3 - Modificar detectives\n"+
-                                "4 - Log In\n"+
-                                "**\nPresione 0 para salir\n**"+
-                                "5 - Listar casos pendientes\n");
-            opcion=sc.nextInt();
-            
-            switch (opcion){
+
+        do {
+            System.out.println("Dectectives\n"
+                    + "1 - Agregar detectives\n"
+                    + "2 - Eliminar detectives\n"
+                    + "3 - Modificar detectives\n"
+                    + "4 - Log In\n"
+                    + "**\nPresione 0 para salir\n**"
+                    + "5 - Listar casos pendientes\n");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
                 case 1:
                     int opcion_detec;
-                    
-                    System.out.println("Agregar Dectectives\n"+
-                                "1 - Crear\n"+
-                                "\n**Presione 0 para regresar**\n");
-                    opcion_detec=sc.nextInt();
-                    
-                    do{
+
+                    System.out.println("Agregar Dectectives\n"
+                            + "1 - Crear\n"
+                            + "\n**Presione 0 para regresar**\n");
+                    opcion_detec = sc.nextInt();
+
+                    do {
                         String nombre, nacionalidad, user, password;
-                        int edad,anios_trabajo;
-                        
+                        int edad, anios_trabajo;
+
                         System.out.println("\nEscriba el nombre: \n");
-                        nombre=sc.next();
+                        nombre = sc.next();
                         System.out.println("Ingrese la edad: \n");
-                        edad=sc.nextInt();
+                        edad = sc.nextInt();
                         System.out.println("Ingrese la nacionalidad: \n");
-                        nacionalidad=sc.next();
+                        nacionalidad = sc.next();
                         System.out.println("Cantidad de años: \n");
-                        anios_trabajo=sc.nextInt();
+                        anios_trabajo = sc.nextInt();
                         System.out.println("Usuario: \n");
-                        user=sc.next();
+                        user = sc.next();
                         System.out.println("Contraseña: \n");
-                        password=sc.next();
-                        
-                                                
-                    }while(opcion_detec!=0);
-                    
+                        password = sc.next();
+
+                        detectives.add(new Detectives());
+
+                    } while (opcion_detec != 0);
+
                     break;
                 case 2:
                     break;
-                case 3: 
+                case 3:
                     break;
                 case 4:
                     break;
                 default:
-                    if(opcion<0 || opcion>4)
-                    System.out.println("Ingrese una opción válida");
+                    if (opcion < 0 || opcion > 4) {
+                        System.out.println("Ingrese una opción válida");
+                    }
                     break;
-                    
+
             }
-        }while (opcion!=0);
-        
-        
-        
+        } while (opcion != 0);
+
     }
 
 }
